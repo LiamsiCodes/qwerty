dca0e11a2f57c6a384bc13e3c411b2bc02be677e
ismail-3-an <ananoucheismail@gmail.com>
2024-02-02 13:33:32 +0100

    delete a problem

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,7 +4,6 @@ import { UserAuth } from '../context/AuthContext'
 function Nav() {
    const {user,logOut}=UserAuth();
    const navigate=useNavigate();
-  //  console.log(user)
 
   const handleLog =async()=>{
     try { 
@@ -16,7 +15,7 @@ function Nav() {
   }
 
   return (
-    <div className='flex items-center p-4 justify-between absolute w-full z-[100]'>
+    <div className='flex items-center p-[15px] justify-between absolute w-full z-[100]'>
       <Link to="/">
       <h1 className='text-red-600 font-extrabold text-2xl md:text-3xl lg:text-4xl'>NETFLIX</h1>
       </Link>
@@ -24,19 +23,19 @@ function Nav() {
     {user?.email ?(
             <div className='mr-2'>
             <Link to="/account">
-            <button className='text-white font-bold pr-4'>Account</button>
+            <button className='text-white font- pr-4'>Account</button>
             </Link>
             <Link to="/signup">
-            <button onClick={handleLog} className='rounded px-6 py-2 font-bold text-white bg-red-600'>Logout</button>
+            <button onClick={handleLog} className='rounded px-6 py-2 font- text-white bg-red-600'>Logout</button>
             </Link>
           </div>
     ):(
       <div className='mr-2'>
       <Link to="/login">
-      <button className='text-white font-bold pr-4'>Sign In</button>
+      <button className='text-white font- pr-4'>Sign In</button>
       </Link>
       <Link to="/signup">
-      <button className='rounded px-6 py-2 font-bold text-white bg-red-600'>Sign Up</button>
+      <button className='rounded px-6 py-2 font- text-white bg-red-600'>Sign Up</button>
       </Link>
     </div>
     )}
